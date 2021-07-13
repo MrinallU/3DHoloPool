e10703f0e65768d09440e2fdfc45f6a6024152ca
michael-glum <61207272+michael-glum@users.noreply.github.com>
2021-07-13 19:12:12 -0400

    commit

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    private GameObject instructions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instructions = GameObject.FindWithTag("instructions");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartPressed()
+    {
+        Debug.Log("ButtonPressed");
+        SceneManager.UnloadScene("Start");
+        SceneManager.LoadScene("Game", LoadSceneMode.Additive);
+    }
+
+    public void InstructionsPressed()
+    {
+        Debug.Log("InstructionsPressed");
+        instructions.SetActive(!instructions.activeInHierarchy);
+    }
+}
