eee567a76d2cab96d9ebe82a5f39610acfb6c140
MrinallU <bossguy124567@gmail.com>
2021-07-31 10:27:12 -0400

    Init Scoring System

---
+++ Assets/Scripts/FloorController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour
+{
+    // update tags accordingly (stripe, solid, or eightBall) 
+    public int score;
+    public bool turn;
+    public string gameStatus;
+    public string playerBallType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        turn = true;
+        gameStatus = "playing";
+        playerBallType = "NA";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameStatus == "lost") // eight scored before all of the others
+        {
+            // lost message message
+            // reset game
+        }
+        else if (gameStatus == "won")
+        {
+            // congrats message
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        updateScore(other.tag);
+        changeTurn();
+    }
+
+    void updateScore(string bt)
+    {
+        // base case
+        if (score == 0)
+        {
+            initBallType(bt);
+        }
+
+        // normal update
+        if (bt == playerBallType)
+        {
+            score++;
+        }
+        else if (bt == "eightBall" && score < 7)
+        {
+            gameStatus = "lost";
+        }
+        else if (bt == "eightBall" && score == 7)
+        {
+            gameStatus = "won";
+        }
+    }
+
+    void changeTurn()
+    {
+        if (turn)
+        {
+            turn = false;
+        }
+        else
+        {
+            turn = true;
+        }
+    }
+
+    void initBallType(string bt)
+    {
+        playerBallType = bt;
+    }
+}
